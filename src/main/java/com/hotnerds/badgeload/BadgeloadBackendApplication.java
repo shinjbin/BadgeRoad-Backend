a21d503cd4f40cece129e3f36595180cc1a4ec03
honghyeong <scarletgold0601@gmail.com>
2022-10-17 22:18:26 +0900

    init project

---
+++ src/main/java/com/hotnerds/badgeload/BadgeloadBackendApplication.java
@@ -0,0 +1,12 @@
+package com.hotnerds.badgeload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadgeloadBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadgeloadBackendApplication.class, args);
+    }
+}
